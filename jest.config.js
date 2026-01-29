92fbe0adbee78823b46537e44a858a504ae28b92
naumanch969 <naumanch969@gmail.com>
2026-01-30 01:01:14 +0500

    test: update jest config with setup and alias support

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,12 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setup.ts"],
   transform: {
     ...tsJestTransformCfg,
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
 };
